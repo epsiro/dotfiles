255f3e1c3de9f9150f0add8a39b34d184c3fbfa6
Bendik S. Søvegjarto <bendik@sovegjarto.com>
2014-10-30 12:46:07 +0100

    symlink_dotfiles.sh: only moving files if they exists

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -11,7 +11,7 @@ dir_colors
 for dotfile in $dotfiles; do
 
     # Backup dotfile if it already exists
-    [ -f ~/.$dotfile ] && mkdir -p ~/dotfiles_old; mv ~/.$dotfile ~/dotfiles_old
+    [ -f ~/.$dotfile ] && mkdir -p ~/dotfiles_old && mv ~/.$dotfile ~/dotfiles_old
 
     # Symlink new dotfile
     ln -s dotfiles/$dotfile ~/.$dotfile
