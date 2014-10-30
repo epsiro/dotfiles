40f522a2cde749aeb6e677243a41ad76a52d6920
Bendik S. Søvegjarto <bendik@sovegjarto.com>
2014-10-30 11:50:20 +0100

    symlink_dotfiles.sh: added

---
+++ symlink_dotfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dotfiles="
+xsession
+Xdefaults
+vimrc
+zshrc
+dir_colors
+"
+
+for dotfile in $dotfiles; do
+
+    # Backup dotfile if it already exists
+    [ -f ~/.$dotfile ] && mkdir -p ~/dotfiles_old; mv ~/.$dotfile ~/dotfiles_old
+
+    # Symlink new dotfile
+    ln -s dotfiles/$dotfile ~/.$dotfile
+    echo .$dotfile symlinked
+done
